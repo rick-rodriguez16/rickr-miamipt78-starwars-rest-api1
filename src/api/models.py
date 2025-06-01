2308a96ad78e2ce383df6a857c421da3a3f73438
Rick Rodriguez <rickrod6502@gmail.com>
2025-06-01 02:34:11 +0000

    adding routes models

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,68 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey, Table, Column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
+# many-to-many relationship between User and Person
+# UserPerson = Table('user_person_favorites',
+#                     db.metadata,
+#                     Column('user_id', ForeignKey('user.id'), primary_key=True, nullable=False),
+#                     Column('person_id', ForeignKey('person.id'), primary_key=True, nullable=False),
+#                     )
+
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
 
+    # relationships
+    user_person: Mapped[list['Favorite_Character']] = relationship(back_populates='user')
+
+    def __repr__(self):
+        return f'<User "{self.username}">'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username,
             # do not serialize the password, its a security breach
+        }
+
+
+class Person(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(120), nullable=True)
+
+    # relationships
+    user_person: Mapped[list['Favorite_Character']] = relationship(back_populates='person')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'hair_color': self.hair_color,
+        }
+
+
+class Favorite_Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    # test: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    # relationships
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    user: Mapped[list['User']] = relationship('User')
+    person_id: Mapped[int] = mapped_column(ForeignKey('person.id'), nullable=False)
+    person: Mapped[list['Person']] = relationship('Person')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "person_id": self.person_id,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Person, Favorite_Character
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -12,11 +12,74 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/users', methods=['GET'])
+def get_people():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    # query the database to get all the starwars characters
+    all_users = User.query.all()
+
+    # take into consideration that there may be None records in the table
+    if all_users is None:
+        return jsonify('Sorry! No users found!'), 404
+    else:
+        all_users = list(map(lambda x: x.serialize(), all_users))
+        return jsonify(all_users), 200
+
+
+@api.route('/users/<int:user_id>/favorites', methods=['GET'])
+def get_user_favorites(user_id):
+
+    current_user = db.session.get(User, user_id)
+
+    if current_user is None:
+        raise APIException('Sorry. User not found', status_code=404)
+    
+    # need to create the logic to retrieve all the users favorites
+    
+    response = {
+        'message': f"Here are {current_user.username}'s favorites: ",
+        'data': "Need to complete serialization of the user's favorites in this route."
     }
+    
+    return jsonify(response), 200
 
-    return jsonify(response_body), 200
+
+
+@api.route('/people', methods=['GET'])
+def get_people():
+
+    # query the database to get all the starwars characters
+    all_people = Person.query.all()
+
+    # take into consideration that there may be None records in the table
+    if all_people is None:
+        return jsonify('Sorry! No Star Wars characters found!'), 404
+    else:
+        all_people = list(map(lambda x: x.serialize(), all_people))
+        return jsonify(all_people), 200
+
+
+@api.route('/people/<int:person_id>', methods=['GET'])
+def get_single_person(person_id):
+
+    # query the database to get a SPECIFIC starwars character by id
+    single_person = db.session.get(Person, person_id)
+
+    if single_person is None:
+        raise APIException(f'Person ID {person_id} was not found!', status_code=404)
+
+    single_person = single_person.serialize()
+    return jsonify(single_person), 200
+
+
+# @api.route('/test', methods=['GET'])
+# def add_fave_person():
+
+#     user1 = db.session.get(User, 2)
+#     print(f'user1 is {user1.id}')
+    
+#     person1 = db.session.get(Person, 1)
+#     print(f'person1 is {person1.id}')
+
+
+#     return jsonify("Let's see if this worked..."), 200
